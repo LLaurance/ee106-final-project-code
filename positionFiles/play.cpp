5f6ac0af4da290b14345937b7821564dfcc94295
ctknizek <112042278+ctknizek@users.noreply.github.com>
2022-12-15 18:52:49 -0800

    Update play.cpp

--- positionFiles/play.cpp
+++ positionFiles/play.cpp
@@ -1,24 +1,40 @@
 #include "play.h"
-#include <Servo.h>
 
 // Define stepper motor connections and motor interface type. Motor interface type must be set to 1 when using a driver:
-#define DIR 13
-#define PUL 15
+#define DIR 6
+#define PUL 8
 #define ENA 37
 #define motorInterfaceType 1
 long stepsPerMM = 400L; 
 int keyLen = 22.5;
-int handArr[3] = {0,-keyLen*stepsPerMM,-2*keyLen*stepsPerMM}; //index 0=left servo's offset
-int handPins[3] = {9,10,11}; //index 0= left servo's pin, etc
+int handArr[3] = {0, -keyLen*stepsPerMM, -2*keyLen*stepsPerMM}; //index 0=left servo's offset
+int handPins[3] = {0,2,4}; //index 0= left servo's pin, etc
 int numWhite = 27;                  
 long currPos = 0L;
-int currNote = 0;
+int currNote = 1;
+long number =0 ;
 
 // Create a new instance of the AccelStepper class:
 
-Servo myservo;
-Servo myservo1;
-Servo myservo2;
+#include <Arduino.h>
+#define USE_PCA9685_SERVO_EXPANDER    // Activating this enables the use of the PCA9685 I2C expander chip/board.
+#define DISABLE_COMPLEX_FUNCTIONS     // Activating this disables the SINE, CIRCULAR, BACK, ELASTIC, BOUNCE and PRECISION easings. Saves up to 1850 bytes program memory.
+#define MAX_EASING_SERVOS 3
+#define ENABLE_EASE_CUBIC
+#include "ServoEasing.hpp"
+#include "PinDefinitionsAndMore.h"
+
+#if defined(USE_PCA9685_SERVO_EXPANDER)
+ServoEasing Servo1(PCA9685_DEFAULT_ADDRESS); // If you use more than one PCA9685 you probably must modify MAX_EASING_SERVOS
+ServoEasing Servo2(PCA9685_DEFAULT_ADDRESS); // If you use more than one PCA9685 you probably must modify MAX_EASING_SERVOS
+ServoEasing Servo3(PCA9685_DEFAULT_ADDRESS); // If you use more than one PCA9685 you probably must modify MAX_EASING_SERVOS
+#else
+ServoEasing Servo1;
+ServoEasing Servo2;
+ServoEasing Servo3;
+#endif
+
+#define START_DEGREE_VALUE  0 // The degree value written to the servo at time of attach.
 
 dist_path shortestPath(int *noteArr, int startIdx, int endIdx, long motorPos) {
   if (startIdx == endIdx) {                                                                       //recursive base case 
@@ -69,42 +85,121 @@ dist_path shortestPath(int *noteArr, int startIdx, int endIdx, long motorPos) {
 
 }
 
-dist_path newShortPath(*int noteArr, int arrLen) {
-  dist_path d;
-  d.dist = 0;
-  d.path = 0;
+long newShortPath(int *noteArr, int arrLen) {
+  long path = 0;
+  long dist = 0;
+  long l = 0;
   for (int i = 0; i<arrLen; i++) {
-    if (getMoveTo(noteArr[i],1) == 0) {
-        d.path = 10 * d.path + 1;
+    if (getMoveTo(noteArr[i],0) == 0) {
+        path = 10 * path + 1;
     }
-    else if (getMoveTo(noteArr[i],0) == 0) {
-        d.path = 10 * d.path + 2;
+    else if (getMoveTo(noteArr[i],1) == 0) {
+        path = 10 * path + 2;
     }
     else if (getMoveTo(noteArr[i],2) == 0) {
-        d.path = 10 * d.path + 3;
+        path = 10 * path + 3;
     }
     else if (noteArr[i]>currNote) {
-        long l = getMoveTo(noteArr[i], 2)
-        d.dist = d.dist + l;
-        d.path = 10* d.path+2;
+        l = getMoveTo(noteArr[i], 2);
+        dist = dist + l;
+        path = 10* path+3;
     }
     else if (noteArr[i]<currNote) {
-        long l = getMoveTo(noteArr[i], 1)
-        d.dist = d.dist + l;
-        d.path = 10* d.path+3;
+        l = getMoveTo(noteArr[i], 0);
+        dist = dist + l;
+        path = 10* path+1;
     }
+    currPos = currPos+l;
     currNote = noteArr[i];
-    Serial.println(currNote);
   }
-  return d;
+  while (path != 0) {
+    number = number * 10;
+    number = number + path % 10;
+    path = path / 10;
+  }
+  currPos = -2L*keyLen*stepsPerMM;
+  path = number;
+  return path;
+}
+ 
+long getMoveTo(int note, int handIdx) {
+  long moveto = 0;
+  long motor_offset = handArr[handIdx];
+  if (note == 0) {
+    moveto = -1;
+  }
+  else if (note<=35) {
+    moveto = (keyLen*(note-1))*stepsPerMM + motor_offset - currPos;
+  }
+  return moveto;
 }
 
+
+/*dist_path newShortPath(int *noteArr, int arrLen) {
+  dist_path d1;
+  d1.dist = 0;
+  d1.path = 0;
+  long l = 0;
+  for (int i = 0; i<arrLen; i++) {
+    if (getMoveTo(noteArr[i],0) == 0) {
+        long x = 10L * d1.path + 1L;
+        d1.path = x;
+    }
+    else if (getMoveTo(noteArr[i],1) == 0) {
+        long x = 10L * d1.path + 2L;
+        d1.path = x;
+    }
+    else if (getMoveTo(noteArr[i],2) == 0) {
+        long x = 10L * d1.path + 3L;
+        d1.path = x;
+    }
+    else if (noteArr[i]>currNote) {
+        long l = getMoveTo(noteArr[i], 2);
+        d1.dist = d1.dist + l;
+        long x = 10L * d1.path + 3L;
+        d1.path = x;
+    }
+    else if (noteArr[i]<currNote) {
+        long l = getMoveTo(noteArr[i], 0);
+        d1.dist = d1.dist + l;
+        long x = 10L * d1.path + 1L;
+        d1.path = x;        
+    }
+    Serial.println(getMoveTo(noteArr[i],1));
+    currNote = noteArr[i];
+    currPos = currPos+l;
+    Serial.println(currNote);
+  }
+  while (d1.path != 0) {
+    number = number * 10;
+    number = number + d1.path % 10;
+    d1.path = d1.path / 10;
+  }
+  d1.path = number;
+  currPos = 0L;
+  return d1;
+}*/
+
 void playNotes(int *noteArr,float *lenArr, int sizeOf) {
-  dist_path d = newShortPath(noteArr, sizeOf);
-  dist_path d;
+  Servo1.attach(handPins[0], 45);
+  Servo2.attach(handPins[1], 45);
+  Servo3.attach(handPins[2], 45);
+  Servo1.setEasingType(EASE_CUBIC_IN_OUT); // EASE_LINEAR is default
+  Servo2.setEasingType(EASE_CUBIC_IN_OUT); // EASE_LINEAR is default
+  Servo3.setEasingType(EASE_CUBIC_IN_OUT); // EASE_LINEAR is default
+
+  for(int i =0; i<sizeOf; i++) {
+    Serial.print("note:");
+    Serial.println(noteArr[i]);
+    Serial.print("len:");
+    Serial.println(lenArr[i]); 
+  }
+  long d = newShortPath(noteArr, sizeOf);
+  
   Serial.print("dist");
-  Serial.println(d.dist);
-  long g = d.path;
+
+  Serial.println(d);
+  long g = d;
   int handIdxArr[sizeOf];
   for (int i = 0; i<sizeOf; i++) {
     handIdxArr[i]= g%10 - 1;
@@ -115,29 +210,35 @@ void playNotes(int *noteArr,float *lenArr, int sizeOf) {
     Serial.println(handIdxArr[i]);
     playNote(noteArr[i],lenArr[i], handIdxArr[i]); 
   }
-  
+  Serial.println("zeroing");
+  for (int i=0; i<currPos+2*keyLen*stepsPerMM; i++) {
+      digitalWrite(DIR,LOW);
+      digitalWrite(ENA,HIGH);
+      digitalWrite(PUL,HIGH);
+      delayMicroseconds(50);
+      digitalWrite(PUL,LOW);
+      delayMicroseconds(50);
+  }
+  Serial.println("done zeroing");
+  currPos = -2*keyLen*stepsPerMM;
 }
-
+/*
 long getMoveTo(int note, int handIdx) {
+  Serial.println(note);
+  Serial.println(handIdx);
   long moveto = 0;
   long motor_offset = handArr[handIdx];
-  if (note<=35) {
-    moveto = (keyLen*note - (keyLen/2))*stepsPerMM + motor_offset - currPos;
+  if (note == 0) {
+    moveto = -1;
   }
-  else {
-    note = note-33;
-    int groupPos = note%5;
-    int group = note/5;
-    int startGrp = keyLen+(group*7*keyLen);
-    if (groupPos<=1){
-      moveto = (startGrp+keyLen*groupPos)*stepsPerMM + motor_offset;
-    }
-    else {
-      moveto = (keyLen*3 + startGrp+keyLen*(groupPos-2))*stepsPerMM + motor_offset;
+  else if (note<=35) {
+    moveto = (keyLen*(note-1))*stepsPerMM + motor_offset - currPos;
+    if (moveto <0){
+      moveto = moveto = keyLen*stepsPerMM;
     }
   }
   return moveto;
-}
+}*/
 
 
 void playNote(int note, float timeToPlay, int handIdx) {
@@ -148,51 +249,41 @@ void playNote(int note, float timeToPlay, int handIdx) {
   Serial.print("moveto:");
   Serial.println(moveto);
   if (moveto>0) {
-    for (int i=0; i<moveto; i++) {
-      digitalWrite(DIR,LOW);
+    digitalWrite(DIR,HIGH);
+  }
+  else {
+    digitalWrite(DIR,LOW);
+  }
+
+  for (int i=0; i<moveto; i++) {
       digitalWrite(ENA,HIGH);
       digitalWrite(PUL,HIGH);
       delayMicroseconds(50);
       digitalWrite(PUL,LOW);
       delayMicroseconds(50);
-    }
   }
-  else {
-    for (int i=0; i<-moveto; i++) {
-        digitalWrite(DIR,HIGH);
-        digitalWrite(ENA,HIGH);
-        digitalWrite(PUL,HIGH);
-        delayMicroseconds(50);
-        digitalWrite(PUL,LOW);
-        delayMicroseconds(50);
-    }
-  }
-
   currPos = currPos + moveto;
+  
   Serial.print("current position:");
   Serial.println( currPos);
-  if (handPins[handIdx] == handPins[0]) {
-    myservo.write(60);
-  }
-  else if (handPins[handIdx] == handPins[1]) {
-    myservo1.write(60);
-  }
-   else if (handPins[handIdx] == handPins[2]) {
-    myservo2.write(90);
-  }
+  
   unsigned long starter = millis();
-  while ((millis() - starter) < timeToPlay*1000) {
-    continue;
-  }
-  Serial.println(0);
-  if (handPins[handIdx] == handPins[0]) {
-    myservo.write(100);
+  if (handIdx ==0) {
+    Servo1.easeTo(120, 200);
+    delay(timeToPlay/10);
+    Servo1.easeTo(80, 200);
+    //myservo.write(70);
   }
   else if (handPins[handIdx] == handPins[1]) {
-    myservo1.write(120);
+    Servo2.easeTo(120, 200);
+    delay(timeToPlay/10);
+    Servo2.easeTo(80, 200);
   }
   else if (handPins[handIdx] == handPins[2]) {
-    myservo2.write(0);
+    Servo3.easeTo(120, 200);
+    delay(timeToPlay/10);
+    Servo3.easeTo(80, 200);
   }
+  delay(1000);
   Serial.println("done");
 }
